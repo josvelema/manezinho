d9377d18b105e0e21c375c0db029b1ca5af4ca9a
josvelema <volumensolutions@gmail.com>
2022-05-09 13:31:00 +0200

    SEO , meta tags added and schema.org scripts

--- assets/js/joscript.js
+++ assets/js/joscript.js
@@ -1,90 +1,140 @@
 // made by Jos Velema 2022
+document.addEventListener("DOMContentLoaded", function () {
+  let lazyVideos = [].slice.call(
+      document.querySelectorAll(".video-slide.lazy")
+  );
 
-//opening hours
-(function (window) {
-  "use strict"
-  function define_openHours () {
+  if ("IntersectionObserver" in window) {
+      let lazyVideoObserver = new IntersectionObserver(function (
+          entries,
+          observer
+      ) {
+          entries.forEach(function (video) {
+              if (video.isIntersecting) {
+                  for (let source in video.target.children) {
+                      let videoSource =
+                          video.target.children[source];
+                      if (
+                          typeof videoSource.tagName ===
+                              "string" &&
+                          videoSource.tagName === "SOURCE"
+                      ) {
+                          videoSource.src =
+                              videoSource.dataset.src;
+                      }
+                  }
 
-    let OpenHours = {}
-    let openClose = document.getElementById('openClosed');
+                  video.target.load();
+                  video.target.classList.remove("lazy");
+                  lazyVideoObserver.unobserve(video.target);
+              }
+          });
+      });
 
-
-    let insertColon = function (time) {
-      let center = time.length - 2
-      let timeWithColon = [time.slice(0, center), ":", time.slice(center)].join("")
-      return timeWithColon
-    }
-
-    let formatTime = function (time) {
-      if (time) {
-        if (time >= 1200) {
-          let formattedTime = time === 1200 ? 1200 : time - 1200
-          return `${insertColon(formattedTime.toString())} PM`
-        } else {
-          return `${insertColon(time.toString())} AM`
-        }
-      }
-    }
-
-    const weekIndex = {
-      0: "Sunday",
-      1: "Monday",
-      2: "Tuesday",
-      3: "Wednesday",
-      4: "Thursday",
-      5: "Friday",
-      6: "Saturday"
-    }
-
-    let determineDay = function (day, dayDiv) {
-      let now = new Date()
-      if (day === weekIndex[now.getDay()]) {
-        dayDiv.style["font-weight"] = "bold"
-        openClose.innerHTML = "We are open!"
-      } else {
-        openClose.innerHTML = "We are closed"
-      }
-
-    }
-
-    OpenHours["generateTime"] = function (hours) {
-      let rootElement = document.getElementById("open-hours")
-      let table = document.createElement("table")
-      if (rootElement) {
-        for (let day in hours) {
-          let dayDiv = document.createElement("tr")
-
-          determineDay(day, dayDiv)
-
-          let dayTitleElement = document.createElement("td")
-          let dayTimesElement = document.createElement("td")
-
-          let dayTitle = document.createTextNode(day)
-          dayTitleElement.appendChild(dayTitle)
-          dayDiv.appendChild(dayTitleElement)
-
-          let dayHours = ""
-
-          if (Object.keys(hours[day]).length === 0) {
-            dayHours = `Closed`
-          } else {
-            dayHours = `${formatTime(hours[day]["start"])} - ${formatTime(hours[day]["end"])}`
-          }
-
-          let dayTimes = document.createTextNode(dayHours)
-          dayTimesElement.appendChild(dayTimes)
-          dayDiv.appendChild(dayTimesElement)
-          table.appendChild(dayDiv)
-        }
-        rootElement.appendChild(table)
-      }
-    }
-
-    return OpenHours
+      lazyVideos.forEach(function (lazyVideo) {
+          lazyVideoObserver.observe(lazyVideo);
+      });
   }
+});
+//Javacript for responsive navigation menu
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
 
-  if (typeof OpenHours === "undefined") {
-    window.OpenHours = define_openHours()
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+//Javacript for video slider navigation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+let sliderNav = function (manual) {
+  btns.forEach((btn) => {
+      btn.classList.remove("active");
+  });
+
+  slides.forEach((slide) => {
+      slide.classList.remove("active");
+  });
+
+  contents.forEach((content) => {
+      content.classList.remove("active");
+  });
+
+  btns[manual].classList.add("active");
+  slides[manual].classList.add("active");
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+      sliderNav(i);
+  });
+});
+
+let currentDate = new Date();
+let weekday = [];
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+if (currentDate.getDay() == "Monday" || "Tuesday") {
+  document
+      .querySelector(".openorclosed")
+      .classList.toggle("closed");
+} else {
+  let currentDay = weekday[currentDate.getDay()];
+
+  let currentTimeHours = currentDate.getHours();
+  currentTimeHours =
+      currentTimeHours < 10
+          ? "0" + currentTimeHours
+          : currentTimeHours;
+  let currentTimeMinutes = currentDate.getMinutes();
+  let timeNow = currentTimeHours + "" + currentTimeMinutes;
+
+  let currentDayID = document.getElementById(currentDay);
+  //gets todays weekday and turns it into id
+  // $(currentDayID).toggleClass("today");
+  currentDayID.classList.toggle("today");
+  //this works at hightlighting today
+
+  // let openTimeSplit = currentDayID.children('.opens').innerText.split(":");
+  let openTimeSplit =
+      currentDayID.children[1].innerText.split(":");
+
+  let openTimeHours = openTimeSplit[0];
+  openTimeHours =
+      openTimeHours < 10 ? "0" + openTimeHours : openTimeHours;
+
+  let openTimeMinutes = openTimeSplit[1];
+  let openTimex = openTimeSplit[0] + openTimeSplit[1];
+
+  //let closeTimeSplit = $(currentDayID).children('.closes').text().split(":");
+  let closeTimeSplit =
+      currentDayID.children[3].innerText.split(":");
+
+  let closeTimeHours = closeTimeSplit[0];
+  closeTimeHours =
+      closeTimeHours < 10 ? "0" + closeTimeHours : closeTimeHours;
+
+  let closeTimeMinutes = closeTimeSplit[1];
+  let closeTimex = closeTimeSplit[0] + closeTimeSplit[1];
+
+  if (timeNow >= openTimex && timeNow <= closeTimex) {
+      //$(".openorclosed").toggleClass("open");
+      document
+          .querySelector(".openorclosed")
+          .classList.toggle("open");
+  } else {
+      document
+          .querySelector(".openorclosed")
+          .classList.toggle("closed");
   }
-}(window))
-
+}
