45b95999c6254f206f4c75571271fbbe027dff3f
josvelema <volumensolutions@gmail.com>
2022-12-19 19:37:28 +0100

    added new about section with new and cropped images

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include "includes/nav.html";
 
 
     <div class="neon">
-      <div class="neon-content">
+      <div class="neon-content rise subheading">
         <p>
 
         </p>
@@ -131,52 +131,6 @@ include "includes/nav.html";
       <button class="btn btn--accent event-btn" onclick="location.href = 'events.php'">Events</button>
     </div>
 
-
-    <article class="content">
-      <h1>Food<br /></h1>
-      <h2><span>Fresh and delicous</span></h2>
-      <p>
-        Try our signature burger , Shrimp in batter or the new
-        T-bone Steak! And for dessert we highly recommend the
-        cheesecake! We recently updated our menu and soon you can
-        have a preview on this site.
-      </p>
-      <!-- <a href="#" class="btn">Read More</a> -->
-    </article>
-    <article class="content">
-      <h1>Drinks<br /></h1>
-      <h2><span>Cold beers , tasty wine and lush cocktails</span></h2>
-      <p>
-        We have all the drinks you got possibly want ! Beer from the
-        tap and bottle , exclusive liquor and ofcourse our signature
-        cocktails!
-      </p>
-      <!-- <a href="#" class="btn">Read More</a> -->
-    </article>
-    <article class="content">
-      <h1>Live Music<br /></h1>
-      <h2><span>Resident and guest artists</span></h2>
-      <p>
-        Besides enjoying a nice dinner and drinks , you're likely to
-        see and hear music played by our artists-in-residence as
-        well as guest players and even open stage nights where
-        everybody can join in the fun! Check out the upcoming events
-        on the event-page!
-      </p>
-      <a href="events.html" class="btn">Events</a>
-    </article>
-    <article class="content">
-      <h1>Art<br /></h1>
-      <h2><span>Expositions</span></h2>
-      <p>
-        Upon visiting Manezinho , you will see a lot of art made by
-        <a href="http://www.pieter-adriaans.com" target="_blank">Pieter Adriaans</a>
-        but also Manezinho acts as a cospace for expositions of
-        works of other people. Check the event page for upcoming
-        events / expositions.
-      </p>
-      <a href="events.html" class="btn">Events</a>
-    </article>
     <aside class="media-icons">
       <a href="https://www.facebook.com/Art-Restaurant-Manezinho-100617402655398" target="_blank">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
